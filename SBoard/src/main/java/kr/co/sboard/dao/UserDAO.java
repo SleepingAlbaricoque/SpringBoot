4336979034c789b3c0b72a299d97ac3e5d6b262b
SleepingAlbaricoque <tnqls0421@naver.com>
2023-01-18 13:51:05 +0900

    2023.01.18 게시판 약관, 회원가입 구현

---
+++ SBoard/src/main/java/kr/co/sboard/dao/UserDAO.java
@@ -0,0 +1,19 @@
+package kr.co.sboard.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import kr.co.sboard.vo.TermsVO;
+import kr.co.sboard.vo.UserVO;
+
+@Mapper
+@Repository
+public interface UserDAO {
+	
+	public int insertUser(UserVO vo); // 영향받는 행의 개수를 return
+	public TermsVO selectTerms();
+	public void selectUser();
+	public void selectUsers();
+	public void updateUser();
+	public void deleteUser();
+}
---
+++ SBoard/src/main/java/kr/co/sboard/vo/TermsVO.java
@@ -0,0 +1,17 @@
+package kr.co.sboard.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class TermsVO {
+	private String terms;
+	private String privacy;
+}
