3ee04cc5113de0a921b11d830f73a8c43fe361bb
SleepingAlbaricoque <tnqls0421@naver.com>
2023-04-17 12:43:34 +0900

    2023.04.17 Voard jwt 실습하기

--- Voard/src/main/java/kr/co/voard/security/SecurityConfig.java
+++ Voard/src/main/java/kr/co/voard/security/SecurityConfig.java
@@ -10,12 +10,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import kr.co.voard.jwt.JWTAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
 	@Autowired
 	private SecurityUserService userService;
+	
+	@Autowired
+	private JWTAuthenticationFilter jwtAuthenticationFilter;
 		
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,9 +31,9 @@ public class SecurityConfig {
 		.csrf().disable()		// 브라우저를 사용하는 환경이 아니기 때문에 비활성
 		.formLogin().disable() 	// 폼 로그인 방식 비활성
 		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS); // JWT 인증을 사용하기 때문에 Session을 끔
-		//.and()
-		//.addFilterBefore(null, null);
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 인증을 사용하기 때문에 Session을 끔
+		.and()
+		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
 	}
